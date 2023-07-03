e5f4cc72defb4b06ecc9a07132256e150ddd5378
aysedinler <aysedinlerrr3@gmail.com>
2023-07-03 22:00:12 +0300

    çalışma4.js

---
+++ hello-world/çalışma4.js
@@ -0,0 +1,62 @@
+// ----------------------------- DERS ORTALAMASI BULMA----------------------------------------
+
+//  vize1 %30 , vize2 %30 , final3 %40
+
+// let vize1 = Number(prompt ("vize 1 notunuzu giriniz :"))
+// let vize2 = Number(prompt ("vize 2 notunuzu giriniz :"))
+
+// let final = Number(prompt("final notunuzu giriniz :"))
+
+// //  kullanıcıdan not bilgilerini aldık. 
+
+// let ortalama = (vize1*0.3) + (vize2*0.3) + (final*0.4)
+// //  işlem önceliği için parantez koyabilirsin.
+// if(ortalama>=60){
+// alert("dersten geçtiniz tebrikler -->"+ ortalama)
+// console.log(" dersten geçtiniz tebrikler")
+
+// }
+// else {
+// alert(" dersten kaldınız -->"+ ortalama)
+// console.log("dersten kaldınız")
+
+// }
+// else if (koşul){}  birden fazla ihtimalin olduğu koşullarda kullanılır.
+// 
+//  Hiçbiri koşulu sağlamazsa else çalışır.         
+
+// --------------------------------- ELSE İF YAPISI-------------------------------------------
+
+// let secilenYol = prompt(" lütfen gitmek istediğiniz yolu seçiniz:")
+// if (secilenYol == 1 ){
+// alert("seçilen yol " + secilenYol+"yoldur.")
+// }
+// else if (secilenYol==2){
+// alert("seçilen yol" + secilenYol + "yoldur.")
+// }
+// else if(secilenYol==3){
+// alert("seçilen yol" + secilenYol+"yoldur.")
+// }
+// else { 
+//     alert("Lütfen geçerli bir yol seçiniz.")
+// }
+
+// kuaför seçimi yapalım
+ let secilenkuaför = prompt(" gitmek istediğiniz kuaförü seçiniz:")
+ if (secilenkuaför ==1){
+    alert ("seçilen kuaför"+ secilenkuaför + "dur.")
+ }
+ else if (secilenkuaför==2){
+    alert("seçilen kuaför" + secilenkuaför + "dur")
+
+ }
+ else if (secilenkuaför==3){
+    alert("seçilen kuaför" + secilenkuaför + "dur")
+ }
+
+ else {
+ alert(" Lütfen geçerli bir kuaför seçiniz!!")
+
+ }
+
+ 
