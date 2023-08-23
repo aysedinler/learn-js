fd9f4a9f41c883e93dcc507a4ce133fa5ac2afef
aysedinler <aysedinlerrr3@gmail.com>
2023-08-23 21:36:02 +0300

    çalışma19.js

---
+++ hello-world/çalışma19.js
@@ -0,0 +1,65 @@
+//  string sınıfının metotları
+/**
+ charAt() :      harfi yazıyor.
+ concat() :      iki tane string ifadeyi birleştirir " " koyarak arayı açabilirsin
+ indexOf():      harfi yazarım bana numarayı verir. charAt ile benzer
+ lastIndexOf():  kelimenin indexini yakalar indexOf'a çok benzer
+ toUpperCase():  hepsini büyük yazar. orijinal stirng değeri değiştirmez.
+ toLowerCase():  aynısının küçük versiyonu
+ trim():         boşlukları temizler tam datayı bana gönderir
+ slice():        harehangi bir değeri çeker (7,10)
+ substring():    (0,5) 5'i almaz 4'e kadar alır
+ eplace():       kelime değişimi yapar ("ayşe" , "umay")
+ split():        dilimler ("") , (" , ")
+ v alueOf(): 
+ startSwıtf():   true /false ayşe a ile mi başlıyor büyük küçük hassasiyeti vardır.
+ endSwith():     bitişe göre true / false
+ */
+
+
+//  ------------------------------------------------------------------------------------
+
+//Math Sınıfı Metotları
+/*
+1-Floor :  noktadan sonrasını atar
+2-Ceil  :  bi üste yuvarlar 3.01 = 4 olur
+3-Round :  en yakın yere yuvarlar 
+
+4-Max   :  number tipinde dizi alır (1,2,3,4,50) sana 50'yi number tipinde döner
+5-Min   :  number tipinde dizi alır en küçüğü döner
+   
+6-Random:  0 ve 1 arasında rastgele bir değer döner.
+7-Abs      Mutlak deger almak için kullanılır |-12| = 12
+8-Sqrt :   karekök almak için kullanılır
+9-Pow  :   üssünü almak için kullanılır
+----> PI Sayisi
+*/
+// console.log(window);
+
+let a = 3.50;
+let b = -12;
+
+// console.log(Math.floor(a));
+// console.log(Math.ceil(a));
+// console.log(Math.round(a));
+
+// console.log(Math.max(1,2,3,4,50));
+// console.log(Math.min(10,0,5));
+// console.log(Math.abs(b));
+
+// console.log(Math.sqrt(169));
+// console.log(Math.pow(5,4));
+
+// console.log(Math.PI);
+
+// let randomDeger = Math.random();
+// let sonuc = randomDeger*100000;
+// let sonucYuvarla = Math.floor(sonuc);
+
+
+
+// console.log(randomDeger);
+// console.log(sonuc);
+// console.log(sonucYuvarla);
+// console.log(Math.floor(Math.random()*100));
+
